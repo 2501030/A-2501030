d2005dd2e732b5d59591425b901500fae036274b
2501030 <shinhyuk25@kbu.ac.kr>
2025-09-24 15:47:58 +0900

    Add files via upload
    3주차

---
+++ weak03/Sample3-3/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        String apple = "부사";  // String apple = new String("부사");
+        byte count = 4;
+        String name = "홍길동";
+        double height = 170.5;
+
+
+        System.out.println("사과의 종류는 부사이고, 개수는 4개입니다.\n", apple, count);
+        System.out.println("%/s님의 키는 %5.if cm 입니다.\n", name, height, '\uf028');
+
+
+
+    }
+}
---
+++ weak03/Sample3-9/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        final String NAME = "양준혁";
+        int batting = 0;
+        int hit = 0;
+        float result = 0.0f;
+
+        System.out.printf("오늘의 타수 입력 : ");
+        batting = keyboard.nextInt();
+        System.out.printf("오늘의 안타수 입력 : ");
+        hit = keyboard.nextInt();
+
+        result =  (float) hit / batting;
+
+        System.out.printf("선수 이름 : %s\n", NAME);
+        System.out.printf("오늘의 타수 : %ds\n", batting);
+        System.out.printf("오늘의 안타수 : %ds\n", hit);
+        System.out.printf("오늘의 타울율 : %.2f\n", result);
+    }
+}
