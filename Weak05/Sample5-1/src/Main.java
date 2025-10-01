3ac6e1a3198ba05da77873116ed6580836f51715
2501030 <shinhyuk25@kbu.ac.kr>
2025-10-01 15:34:50 +0900

    5주차

---
+++ Weak05/Sample5-1/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+          boolean test = 5 > 4;
+          boolean test1 = 'A' > 'a';
+          boolean test2 = 'b' + 1 < 'a';
+          boolean test3 = 0 == -0;
+          boolean test4 = true == false;
+
+          System.out.println(test);
+          System.out.println(test1);
+          System.out.println(test2);
+          System.out.println(test3);
+          System.out.println(test4);
+    }
+}
